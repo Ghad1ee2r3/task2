8e773c9b0530325432d6057adaf8b56bd47c079e
ghadeer <apps02977@gmail.com>
2020-09-24 14:17:13 +0300

    done

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+let opration= '/'
+let num1=8;
+let num2=2;
+
+if (opration=="+"){
+    console.log(`Result of ${num1} + ${num2}= ${num1+num2}`);
+}else if (opration=="-"){
+    console.log(`Result ${num1} - ${num2}= ${num1-num2}`);
+}else if(opration=="*")
+{
+    console.log(`Result ${num1} * ${num2}= ${num1*num2}`);
+
+}else if(opration=="/"){
+    console.log(`Result ${num1} / ${num2}= ${num1/num2}`);
+
+}
